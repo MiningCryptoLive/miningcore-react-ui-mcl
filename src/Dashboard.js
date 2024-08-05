0e06d518cbe0a0b774a1a5b1f805cb0fe8cd5279
MiningCryptoLive <celldr69@gmail.com>
2024-08-05 10:53:07 -0400

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -68,8 +68,8 @@ export default () => (
             Information
           </Typography>
           <Typography variant="h5" component="h2">
-            <a href="https://blockchair.com/dogecoin" target="_blank" style="color:white"> Block Explorer </a>
-            <a href="https://blockchair.com/dogecoin" target="_blank" style="color:white"> Network Stats </a>
+            <a href="https://blockchair.com/dogecoin" target="_blank"> Block Explorer </a><br>
+            <a href="https://blockchair.com/dogecoin" target="_blank"> Network Stats </a>
           </Typography>
           <Typography  color="textSecondary">
             adjective
